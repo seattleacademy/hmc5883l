2d1a665b436252168bfc5786f8e5f5c908d68c4d
Aidan Wood <aidanwood5@outlook.com>
2015-04-20 09:21:44 -0700

    fix gitignore issue, add correct files

---
+++ example.js
@@ -0,0 +1,15 @@
+var hmc5883l = require('hmc5883l');
+
+var compass = new hmc5883l({device: '/dev/i2c-1'});
+
+function outCompass(){
+  compass.readXYZ(function(err, data) {
+      if (err) {
+          console.log(err);
+      }
+      else {
+          console.log(data);
+      }
+  });
+  setTimeout(outCompass, 1000);
+}
